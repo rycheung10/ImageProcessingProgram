f5d605f4510b7480f664fde06cda58245fd897d1
Ryan Cheung <ry.cheung@MacBook-Pro-343.local>
2022-11-08 17:17:01 -0500

    new save and load methods, no tests

--- src/ImageProcessing.java
+++ src/ImageProcessing.java
@@ -20,9 +20,9 @@ public class ImageProcessing {
   public static void main(String[] args) {
     IPModel m1 = new IPModelImpl();
     IPView v1 = new IPViewImpl();
-    Readable is = new StringReader("load res/techsupport.ppm ts q");
+    Readable is = new StringReader("load res/techsupport.ppm ts save /users/ry.cheung/desktop/techsupportpng.jpg ts q");
     Readable re = new InputStreamReader(System.in);
-    IPController c1 = new IPControllerImpl(m1, v1, re);
+    IPController c1 = new IPControllerImpl(m1, v1, is);
     
     c1.startIP();
   }
