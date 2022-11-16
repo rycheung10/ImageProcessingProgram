8d37c75e0ff298ee25e095149f6c148a851b1bf9
Ryan Cheung <ry.cheung@MacBook-Pro-343.local>
2022-11-16 12:50:26 -0500

    render an image

--- src/ImageProcessingWGUI.java
+++ src/ImageProcessingWGUI.java
@@ -5,7 +5,7 @@ import view.IPViewGUIImpl;
 public class ImageProcessingWGUI {
   public static void main (String[] args) {
     IPModelImpl m1 = new IPModelImpl();
-    IPViewGUIImpl v1 = new IPViewGUIImpl();
+    IPViewGUIImpl v1 = new IPViewGUIImpl(m1);
     IPControllerGUIImpl c1 = new IPControllerGUIImpl(m1, v1);
     c1.startIPGUI();
   }
