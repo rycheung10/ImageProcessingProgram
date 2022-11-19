a001679ba4c115dd0dbeaacba6282faffa8dbcda
ethankong150 <kong.e@northeastern.edu>
2022-11-19 14:21:50 -0500

    rework of histogramIMPL

--- src/view/IPHistogram.java
+++ src/view/IPHistogram.java
@@ -4,6 +4,5 @@ import java.util.Map;
 
 public interface IPHistogram {
 
-  void addData(Map<Integer, Integer> redData, Map<Integer, Integer> greenData,
-                      Map<Integer, Integer> blueData, Map<Integer, Integer> intensityData);
+  void createHistogramModel(String imgName);
 }
