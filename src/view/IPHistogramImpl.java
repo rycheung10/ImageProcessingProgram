1d6f36b2b4d27b41e6d1cb6d10ce275eccaac1dd
ethankong150 <kong.e@northeastern.edu>
2022-11-19 14:38:46 -0500

    frequency data scales with size of img and height of graph

--- src/view/IPHistogramImpl.java
+++ src/view/IPHistogramImpl.java
@@ -8,7 +8,6 @@ import java.util.Map;
 import javax.swing.*;
 
 import model.IPModel;
-import model.IPModelState;
 import model.IPModelState.PixelComponents;
 
 import static model.IPModelState.PixelComponents.Blue;
@@ -17,7 +16,7 @@ import static model.IPModelState.PixelComponents.Intensity;
 import static model.IPModelState.PixelComponents.Red;
 
 //!!!!!!!! each histogram graph is 375 x 250
-public class IPHistogramImpl extends JPanel implements IPHistogram{
+public class IPHistogramImpl extends JPanel implements IPHistogram {
 
   ArrayList<DataPoint> redBarsData;
   ArrayList<DataPoint> greenBarsData;
@@ -38,6 +37,7 @@ public class IPHistogramImpl extends JPanel implements IPHistogram{
     this.height = height;
     this.width = width;
     this.model = model;
+
   }
 
 
@@ -49,44 +49,54 @@ public class IPHistogramImpl extends JPanel implements IPHistogram{
     Map<Integer, Integer> blueData = collectData(Blue, imgName);
     Map<Integer, Integer> intensityData = collectData(Intensity, imgName);
 
-      for(int i = 0; i <= 255; i++) {
-        if(redData.containsKey(i)) {
-          this.redBarsData.add(new DataPoint(Color.red, (redData.get(i) / height) * 100, 1));
-        }
-      }
+    int totalPixelsInImg = this.model.getHeight(imgName) * this.model.getWidth(imgName);
 
-      for(int j = 0; j <= 255; j++) {
-        if(greenData.containsKey(j)) {
-          this.greenBarsData.add(new DataPoint(Color.green, (greenData.get(j) / height) * 100, 1));
-        }
+    for (int i = 0; i <= 255; i++) {
+      if (redData.containsKey(i)) {
+        this.redBarsData.add(new DataPoint(Color.red, (redData.get(i) / totalPixelsInImg) * this.height, 1));
+      } else {
+        this.redBarsData.add(new DataPoint(Color.red, 0, 1));
       }
+    }
 
-      for(int k = 0; k <= 255; k++) {
-        if(blueData.containsKey(k)) {
-          this.blueBarsData.add(new DataPoint(Color.blue, (blueData.get(k) / height) * 100, 1));
-        }
+    for (int j = 0; j <= 255; j++) {
+      if (greenData.containsKey(j)) {
+        this.greenBarsData.add(new DataPoint(Color.green, (greenData.get(j) / totalPixelsInImg) * this.height, 1));
+      } else {
+        this.greenBarsData.add(new DataPoint(Color.green, 0, 1));
       }
+    }
 
-      for(int l = 0; l <= 255; l++) {
-        if(intensityData.containsKey(l)) {
-          this.intensityBarsData.add(new DataPoint(Color.black, greenData.get(l), 1));
-        }
+    for (int k = 0; k <= 255; k++) {
+      if (blueData.containsKey(k)) {
+        this.blueBarsData.add(new DataPoint(Color.blue, (blueData.get(k) / totalPixelsInImg) * this.height, 1));
+      } else {
+        this.blueBarsData.add(new DataPoint(Color.blue, 0, 1));
       }
+    }
+
+    for (int l = 0; l <= 255; l++) {
+      if (intensityData.containsKey(l)) {
+        this.intensityBarsData.add(new DataPoint(Color.black, (intensityData.get(l) / totalPixelsInImg) * this.height, 1));
+      } else {
+        this.intensityBarsData.add(new DataPoint(Color.black, 0, 1));
+      }
+    }
   }
 
   private Map<Integer, Integer> collectData(PixelComponents component, String imgName) {
-      Map<Integer, Integer> data = new HashMap<>();
-      for(int i = 0; i < this.model.getHeight(imgName); i++) {
-        for(int j = 0; j < this.model.getWidth(imgName); j++) {
-          int currNum = this.model.getPixelInfo(imgName, i, j).get(component);
-          if(data.containsKey(currNum)) {
-            data.put(currNum, data.get(currNum) + 1);
-          } else {
-            data.put(currNum, 1);
-          }
+    Map<Integer, Integer> data = new HashMap<>();
+    for (int i = 0; i < this.model.getHeight(imgName); i++) {
+      for (int j = 0; j < this.model.getWidth(imgName); j++) {
+        int currNum = this.model.getPixelInfo(imgName, i, j).get(component);
+        if (data.containsKey(currNum)) {
+          data.put(currNum, data.get(currNum) + 1);
+        } else {
+          data.put(currNum, 1);
         }
       }
-      return data;
+    }
+    return data;
   }
 
 }
